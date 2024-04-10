540155865c524e128c3f3cee3be1a38f5343a9f5
naybala <naybala.zotefamily@gmail.com>
2024-04-10 22:03:16 +0630

    test pkg ms sql

--- NBLDotNetCore.ConsoleApp/Program.cs
+++ NBLDotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "DAVION"; //server name
+stringBuilder.InitialCatalog = "DotNetTrainning";
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sa@123";
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+connection.Open();
+Console.WriteLine("Open is Here");
+connection.Close();
+Console.WriteLine("Close is Here");
+Console.ReadKey();
