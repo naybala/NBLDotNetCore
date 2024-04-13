96f4ca95c6443364902557b744cb31debf7da60b
naybala <naybala.zotefamily@gmail.com>
2024-04-13 22:51:26 +0630

    Insert new data

---
+++ NBLDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBLDotNetCore.ConsoleApp
+{
+    internal class AdoDotNetExample
+    {
+
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "DAVION",
+            InitialCatalog = "DotNetTrainning",
+            UserID = "sa",
+            Password = "sa@123"
+        };
+
+        private string Result(int result,string processType)
+        {
+           return result > 0 ? processType+" Success" : processType+" Failed";
+        }
+
+        public void Read()
+        {            
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            Console.WriteLine("Open is Here");
+            string query = "select * from Tbl_Blog";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            connection.Close();
+            Console.WriteLine("Close is Here");
+            Console.WriteLine("--------------------------------------------");
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine(dr["BlogId"]);
+                Console.WriteLine(dr["BlogTitle"]);
+                Console.WriteLine(dr["BlogAuthor"]);
+                Console.WriteLine(dr["BlogContent"]);
+                Console.WriteLine("--------------------------------------------");
+
+            }
+        }
+
+        public void Insert(string title,string author , string content)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+                           ([BlogTitle]
+                           ,[BlogAuthor]
+                           ,[BlogContent])
+                     VALUES
+                           (@BlogTitle,
+		                   @BLogAuthor,
+		                   @BlogContent)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BLogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+            string message = Result(result,"Inserting");
+            Console.WriteLine(message);
+        }
+
+        
+    }
+}
--- NBLDotNetCore.ConsoleApp/Program.cs
+++ NBLDotNetCore.ConsoleApp/Program.cs
@@ -1,34 +1,9 @@
-﻿using System.Data;
+﻿using NBLDotNetCore.ConsoleApp;
+using System.Data;
 using System.Data.SqlClient;
 
-Console.WriteLine("Hello, World!");
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = "DAVION"; //server name
-stringBuilder.InitialCatalog = "DotNetTrainning";
-stringBuilder.UserID = "sa";
-stringBuilder.Password = "sa@123";
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
-connection.Open();
-Console.WriteLine("Open is Here");
-
-string query = "select * from Tbl_Blog";
-SqlCommand cmd = new SqlCommand(query, connection);
-SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-DataTable dt = new DataTable();
-sqlDataAdapter.Fill(dt);
-
-
-connection.Close();
-Console.WriteLine("Close is Here");
-
-foreach(DataRow dr in dt.Rows)
-{
-    Console.WriteLine(dr["BlogId"]);
-    Console.WriteLine(dr["BlogTitle"]);
-    Console.WriteLine(dr["BlogAuthor"]);
-    Console.WriteLine(dr["BlogContent"]);
-    Console.WriteLine("--------------------------------------------");
-
-}
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+///adoDotNetExample.Read();
+adoDotNetExample.Insert("U Mya Book","U Mya","This is Content");
 
 Console.ReadKey();
