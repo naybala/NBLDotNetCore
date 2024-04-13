416ff1a8c77694bbf8babb119984cc9934c25ce0
naybala <naybala.zotefamily@gmail.com>
2024-04-13 22:20:31 +0630

    data read

--- NBLDotNetCore.ConsoleApp/Program.cs
+++ NBLDotNetCore.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
 SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
@@ -9,6 +10,25 @@ stringBuilder.Password = "sa@123";
 SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
 connection.Open();
 Console.WriteLine("Open is Here");
+
+string query = "select * from Tbl_Blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+
+
 connection.Close();
 Console.WriteLine("Close is Here");
+
+foreach(DataRow dr in dt.Rows)
+{
+    Console.WriteLine(dr["BlogId"]);
+    Console.WriteLine(dr["BlogTitle"]);
+    Console.WriteLine(dr["BlogAuthor"]);
+    Console.WriteLine(dr["BlogContent"]);
+    Console.WriteLine("--------------------------------------------");
+
+}
+
 Console.ReadKey();
