81d29d855ea0d0248747f202bef8fba1bbaaa6c1
naybala <naybala.zotefamily@gmail.com>
2024-04-28 16:04:01 +0630

    Dapper Create

--- NBLDotNetCore.ConsoleApp/DapperExample.cs
+++ NBLDotNetCore.ConsoleApp/DapperExample.cs
@@ -7,28 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NBLDotNetCore.ConsoleApp
-{
-    internal class DapperExample
-    {
-        public void Run()
-        {
-            Read();
-        }
+namespace NBLDotNetCore.ConsoleApp;
 
-        private void Read()
+internal class DapperExample
+{
+    public void Run()
+    {
+        Read();
+        Edit(1);
+        Create("U Mya Book", "U Mya", "This is Content");
+    }
+
+    private void Read()
+    {
+        using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        List<BlogDto> lst =  db.Query<BlogDto>("Select * from tbl_blog").ToList();
+        foreach (BlogDto item in lst)
         {
-            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
-            List<BlogDto> lst =  db.Query<BlogDto>("Select * from tbl_blog").ToList();
-            foreach (BlogDto item in lst)
-            {
-                Console.WriteLine(item.BlogId);
-                Console.WriteLine(item.BlogTitle);
-                Console.WriteLine(item.BlogAuthor);
-                Console.WriteLine(item.BlogContent);
-                Console.WriteLine("=============================================");
-            }
+            Console.WriteLine(item.BlogId);
+            Console.WriteLine(item.BlogTitle);
+            Console.WriteLine(item.BlogAuthor);
+            Console.WriteLine(item.BlogContent);
+            Console.WriteLine("=============================================");
         }
+    }
+
+    private void Edit(int id)
+    {
+        using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        var item = db.Query<BlogDto>("Select * from tbl_blog where BlogId = @BlogId", new BlogDto { BlogId = id }).FirstOrDefault();
+        if(item is null)
+        {
+            Console.WriteLine("No Data Found");
+            return;
+        }
+        Console.WriteLine(item.BlogId);
+        Console.WriteLine(item.BlogTitle);
+        Console.WriteLine(item.BlogAuthor);
+        Console.WriteLine(item.BlogContent);
+        Console.WriteLine("=============================================");
+    }
+
+    private void Create(string title, string author, string content)
+    {
+        var item = new BlogDto
+        {
+            BlogTitle = title,
+            BlogAuthor = author,
+            BlogContent = content
+        };
+
+        string query = @"INSERT INTO [dbo].[Tbl_Blog]
+                           ([BlogTitle]
+                           ,[BlogAuthor]
+                           ,[BlogContent])
+                     VALUES
+                           (@BlogTitle,
+		                   @BLogAuthor,
+		                   @BlogContent)";
+
+        using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        int result = db.Execute(query, item);
+        string message = result > 0 ? "Insert Success." : "Inserting Failed.";
+        Console.WriteLine(message);
 
     }
+
 }
