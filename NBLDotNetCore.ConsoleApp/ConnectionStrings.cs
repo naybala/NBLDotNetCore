a552c9437beeea222961a724352a1262aadee17f
naybala <naybala.zotefamily@gmail.com>
2024-04-28 15:38:52 +0630

    implement Dapper
    Read

---
+++ NBLDotNetCore.ConsoleApp/ConnectionStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBLDotNetCore.ConsoleApp
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "DAVION",
+            InitialCatalog = "DotNetTrainning",
+            UserID = "sa",
+            Password = "sa@123"
+        };
+
+    }
+}
--- NBLDotNetCore.ConsoleApp/Program.cs
+++ NBLDotNetCore.ConsoleApp/Program.cs
@@ -2,10 +2,14 @@
 using System.Data;
 using System.Data.SqlClient;
 
-AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
 ///adoDotNetExample.Read();
 //adoDotNetExample.Insert("U Mya Book","U Mya","This is Content");
 //adoDotNetExample.Update(1,"U Mya One Book", "U Mya One", "This is Content One");
 //adoDotNetExample.Delete(5);
-adoDotNetExample.Edit(1);
+//adoDotNetExample.Edit(1);
+
+
+DapperExample dapperExample = new DapperExample();
+dapperExample.Run();
 Console.ReadKey();
