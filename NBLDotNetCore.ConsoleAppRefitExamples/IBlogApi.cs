2f8b0884f46918fa41f7e0a4d7313c2bbad2d48a
naybala <naybala.zotefamily@gmail.com>
2024-05-26 18:15:01 +0630

    refit examples

---
+++ NBLDotNetCore.ConsoleAppRefitExamples/IBlogApi.cs
@@ -0,0 +1,30 @@
+﻿using Refit;
+
+namespace NBLDotNetCore.ConsoleAppRefitExamples
+{
+    public interface IBlogApi
+    {
+        [Get("/api/blog")]
+        Task<List<BlogModel>> GetBlogs();
+
+        [Get("/api/blog/{id}")]
+        Task<BlogModel> GetBlog(int id);
+
+        [Post("/api/blog")]
+        Task<string> CreateBlog(BlogModel blog);
+
+        [Put("/api/blog/{id}")]
+        Task<string> UpdateBlog(int id, BlogModel blog);
+
+        [Delete("/api/blog/{id}")]
+        Task<string> DeleteBlog(int id);
+    }
+
+    public class BlogModel
+    {
+        public int BlogId { get; set; }
+        public string? BlogTitle { get; set; }
+        public string? BlogAuthor { get; set; }
+        public string? BlogContent { get; set; }
+    }
+}
---
+++ NBLDotNetCore.ConsoleAppRefitExamples/Program.cs
@@ -0,0 +1,15 @@
+﻿using NBLDotNetCore.ConsoleAppRefitExamples;
+
+//try
+//{
+//    RefitExample refitExample = new RefitExample();
+//    await refitExample.RunAsync();
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.ToString());
+//}
+
+
+RefitExample refitExample = new RefitExample();
+await refitExample.RunAsync();
